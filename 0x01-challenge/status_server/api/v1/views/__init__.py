081525fc68d68473a74d78e5460347847dd15190
Olusegun <ayosegun2001@gmail.com>
2024-05-08 17:26:13 +0100

    0x01. Fix my code: Debugging

--- 0x01-challenge/status_server/api/v1/views/__init__.py
+++ 0x01-challenge/status_server/api/v1/views/__init__.py
@@ -2,5 +2,6 @@
 """ Views module
 """
 from flask import Blueprint
-from api.v1.views.index import *
 app_views = Blueprint("app_views", __name__, url_prefix="/")
+
+from api.v1.views.index import *
